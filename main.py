2fc742a01fc55d5c1a010bc860f86f90df074b5b
Caz Moore <cazmoore@users.noreply.github.com>
2023-04-15 17:38:24 -0500

    Revert "Changed to use csv input file"
    This reverts commit e51e90fc06821a944796128f0bd1f88800fecf8d.

--- main.py
+++ main.py
@@ -5,14 +5,15 @@ from notifications import send_push_notification
 # Get the current date
 today = datetime.datetime.now().date()
 
-# Load the CSV file using pandas
-df = pd.read_csv('workouts.csv')
+# Load the Excel file using pandas
+df = pd.read_excel('workouts.xlsx')
 
 # Convert the date column to datetime format with dayfirst=True
 df['Date'] = pd.to_datetime(df['Date'], format='%d/%m/%Y')
 
 # Filter the dataframe to get the row with the current date
 current_workout = df[df['Date'].dt.date == today]['Workout'].values
+
 # Check if a workout exists for the current date
 if len(current_workout) > 0:
     message = f"Good morning Caroline. Today is {today.strftime('%-d %B')}, and your workout is: {current_workout[0]}."
